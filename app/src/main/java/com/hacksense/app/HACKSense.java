f5fba532229208b90015a8df4ea91129cfccee63
karak1974 <wolfy42@protonmail.com>
2023-12-24 01:32:19 +0100

    Draft

--- app/src/main/java/com/hacksense/app/HACKSense.java
+++ app/src/main/java/com/hacksense/app/HACKSense.java
@@ -3,12 +3,20 @@ package com.hacksense.app;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Implementation of App Widget functionality.
@@ -21,10 +29,8 @@ public class HACKSense extends AppWidgetProvider {
         return sdf.format(new Date());
     }
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        // Get the app name
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) throws ExecutionException, InterruptedException {
         CharSequence widgetText = context.getString(R.string.app_name);
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.h_a_c_k_sense);
         views.setTextViewText(R.id.widget_title, widgetText);
 
@@ -34,15 +40,22 @@ public class HACKSense extends AppWidgetProvider {
         Log.i("HACKSense", "When "+when);
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
+        String responseBody = new HttpRequestTask(context, appWidgetManager, appWidgetId, views).execute().get();
+        Log.i("HACKSense", responseBody);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            try {
+                updateAppWidget(context, appWidgetManager, appWidgetId);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-        Log.d("HACKSense", "Update trigger");
+        Log.i("HACKSense", "Update triggered");
     }
 
     @Override
@@ -53,5 +66,54 @@ public class HACKSense extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    private static class HttpRequestTask extends AsyncTask<Void, Void, String> {
+        private final Context context;
+        private final AppWidgetManager appWidgetManager;
+        private final int appWidgetId;
+        private final RemoteViews views;
+
+        public HttpRequestTask(Context context, AppWidgetManager appWidgetManager, int appWidgetId, RemoteViews views) {
+            this.context = context;
+            this.appWidgetManager = appWidgetManager;
+            this.appWidgetId = appWidgetId;
+            this.views = views;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // Perform the GET request
+            try {
+                URL url = new URL("https://vsza.hu/hacksense/status.json");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    InputStream in = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    return response.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (IOException e) {
+                Log.e("HACKSense", "Error making GET request", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // Update the widget views with the response body
+            if (result != null) {
+                Log.i("HACKSense", "Response Body: " + result);
+                // You can update the widget views with the response here
+                // For example: views.setTextViewText(R.id.response_body, result);
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+            }
+        }
     }
 }
